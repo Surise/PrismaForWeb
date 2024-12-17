f075800e25146d364328ce8d77f13fd4dfaf8aea
Surise <2858819642@qq.com>
2024-12-17 18:45:43 +0800

    镇楼

--- js/main.js
+++ js/main.js
@@ -9,6 +9,41 @@ async function checkPort(port) {
         throw new Error(`Port ${port} not accessible`); // 显式抛出错误
     }
 }
+//                        d*##$.
+// # zP"""""$e.           $"    $o
+// #4$       '$          $"      $
+// #'$        '$        J$       $F
+// # 'b        $k       $>       $
+// #  $k        $r     J$       d$
+// #  '$         $     $"       $~
+// #   '$        "$   '$E       $
+// #    $         $L   $"      $F ...
+// #     $.       4B   $      $$$*"""*b
+// #     '$        $.  $$     $$      $F
+// #      "$       R$  $F     $"      $
+// #       $k      ?$ u*     dF      .$
+// #       ^$.      $$"     z$      u$$$$e
+// #        #$b             $E.dW@e$"    ?$
+// #         #$           .o$$# d$$$$c    ?F
+// #          $      .d$$#" . zo$>   #$r .uF
+// #          $L .u$*"      $&$$$k   .$$d$$F
+// #           $$"            ""^"$$$P"$P9$
+// #          JP              .o$$$$u:$P $$
+// #          $          ..ue$"      ""  $"
+// #         d$          $F              $
+// #         $$     ....udE             4B
+// #          #$    """"` $r            @$
+// #           ^$L        '$            $F
+// #             RN        4N           $
+// #              *$b                  d$
+// #               $$k                 $F
+// #               $$b                $F
+// #                 $""               $F
+// #                 '$                $
+// #                  $L               $
+// #                  '$               $
+// #                   $               $
+// #
 let successfulPort=14250;
 async function findOpenPort() {
     const portRange = Array.from({ length: 11 }, (_, i) => 14250 + i); // 14250 到 14260
